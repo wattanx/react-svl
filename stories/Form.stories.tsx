d47cb3bc7fd49d4b63826c15dfb1fe6759d901ed
wattanx <wattan.dev@gmail.com>
2021-09-02 00:33:52 +0900

    StorybookにChakra UIを設定

---
+++ stories/Form.stories.tsx
@@ -0,0 +1,48 @@
+import { Meta } from '@storybook/react';
+import { Box } from '@chakra-ui/layout';
+import { Input } from '@chakra-ui/input';
+import { FormControl, FormErrorMessage, FormLabel } from '@chakra-ui/form-control';
+import { useForm } from '../src';
+
+export default {
+  title: 'Example/FormValidation',
+} as Meta;
+
+export const ValdiationExample = () => {
+  const { errors, values, setFieldValue, validateField } = useForm({
+    initialValues: {
+      FirstName: '',
+      Password: '',
+    },
+    validationRules: {
+      FirstName: {
+        isRequired: true,
+      },
+      Password: {
+        minLength: 8,
+      },
+    },
+  });
+  return (
+    <Box>
+      <FormControl isInvalid={errors.FirstName.isInValid}>
+        <FormLabel>First Name</FormLabel>
+        <Input
+          value={values.FirstName}
+          onChange={(e) => setFieldValue('FirstName', e.currentTarget.value)}
+          onBlur={() => validateField('FirstName')}
+        />
+        <FormErrorMessage>{errors.FirstName.isRequired?.message}</FormErrorMessage>
+      </FormControl>
+      <FormControl isInvalid={errors.Password.isInValid}>
+        <FormLabel>Password</FormLabel>
+        <Input
+          value={values.Password}
+          onChange={(e) => setFieldValue('Password', e.currentTarget.value)}
+          onBlur={() => validateField('Password')}
+        />
+        <FormErrorMessage>{errors.Password.minLength?.message}</FormErrorMessage>
+      </FormControl>
+    </Box>
+  );
+};
